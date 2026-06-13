0e266453f09f838c655628d82863ad5d10e5ba2d
Fuma Nama <god63820869@gmail.com>
2026-06-13 14:05:17 +0800

    use Fumapress

--- apps/docs/source.config.ts
+++ apps/docs/source.config.ts
@@ -1,7 +1,16 @@
-import { defineDocs, defineConfig } from "fumadocs-mdx/config";
+import { defineDocs } from "fumadocs-mdx/config";
+import { metaSchema, pageSchema } from "fumapress/adapters/mdx/schema";
 
 export const docs = defineDocs({
-  dir: "content/docs",
+  dir: "content",
+  docs: {
+    async: true,
+    schema: pageSchema,
+    postprocess: {
+      includeProcessedMarkdown: true,
+    },
+  },
+  meta: {
+    schema: metaSchema,
+  },
 });
-
-export default defineConfig();
